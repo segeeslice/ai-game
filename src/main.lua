3ca90d30ca5b378e78318be9129296d594666085
Dustin Seger <dustin.seger@hotmail.com>
2020-07-11 22:32:47 +0000

    Import basic LOVE scripts and add main.lua

---
+++ src/main.lua
@@ -0,0 +1,26 @@
+-- ** Global Definitions **
+
+-- Global window object for more efficient storing of data
+-- TODO: to width/height
+WIN = {
+  x = {
+    size = 0
+  },
+  y = {
+    size = 0
+  }
+}
+
+-- ** Love callbacks **
+
+function love.load()
+end
+
+function love.resize(w, h)
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
