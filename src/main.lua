7975896b7eed789ec965409587102ac3e3aa4535
Dustin Seger <dustin.seger@hotmail.com>
2020-07-19 21:16:05 +0000

    Set up basic window size tracking

--- src/main.lua
+++ src/main.lua
@@ -1,25 +1,26 @@
 -- ** Global Definitions **
 
--- Global window object for more efficient storing of data
--- TODO: to width/height
-WIN = {
-  x = {
-    size = 0
-  },
-  y = {
-    size = 0
-  }
-}
-
 -- ** Love callbacks **
 
 function love.load()
+  love.window.setMode(0, 0, {
+    resizable = true,
+    minwidth = 800,
+    minheight = 600
+  })
+  math.randomseed(os.time())
+
+  love.resize(love.graphics.getDimensions())
 end
 
+-- Width & height aren't default love variables, but this
+-- allows for less function calls as opposed to getDimensions()
 function love.resize(w, h)
+  love.window.width, love.window.height = w, h
 end
 
 function love.draw()
+
 end
 
 function love.update(dt)
