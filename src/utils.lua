ac547dfdb880b9fe4849ca37c58c3e3540a62da4
Dustin Seger <dustin.seger@hotmail.com>
2021-04-26 20:16:09 -0400

    Enable debug logging with robust table parsing

--- src/utils.lua
+++ src/utils.lua
@@ -27,9 +27,43 @@ function nRoot(num, root)
   return math.pow(num, 1/root)
 end
 
+-- Recursive function to traverse tables and gather string of contents
+-- https://stackoverflow.com/questions/41942289/display-contents-of-tables-in-lua/41942900
+function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\r\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\r\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
+end
+
+-- Print all the values within a table via tprint
+function pprint (obj)
+  print(tprint(obj))
+end
+
 -- Namespace for easier code tracing
 utils = {
   copy = copy,
   logBase = logBase,
-  nRoot = nRoot
+  nRoot = nRoot,
+  tprint = tprint,
+  pprint = pprint,
 }
